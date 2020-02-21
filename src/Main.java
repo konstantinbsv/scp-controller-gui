a6dcc3e225857ffd52a79ddd09bf7484ca14815a
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-02-21 09:03:05 -0800

    Launch ControllerGUI from Main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // launch GUI
+        Application.launch(ControllerGUI.class, args);
+    }
+}
