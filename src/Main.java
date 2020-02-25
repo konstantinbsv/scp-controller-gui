3f6504ab90884dc0897e6006feb8b0eee84276a0
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-02-25 09:04:22 -0800

    Move serial communication methods

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
+import com.fazecast.jSerialComm.SerialPort;
 import javafx.application.Application;
+import javafx.application.Platform;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // launch GUI
-        Application.launch(ControllerGUI.class, args);
+        // get GUIModel instance
+
+        Application.launch(GUI.class, args);
+//        Runnable gui = new GUI();
+//        Thread guiThread = new Thread(gui);
+//        guiThread.run();
+
+        // open COM port
+        // SerialComm stm32Comm = new SerialComm(SerialComm.comPort);
     }
+
 }
