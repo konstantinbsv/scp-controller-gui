ba229bc298b89562fae9d188c5497fd4ab7d9da1
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-02-26 11:06:10 -0800

    Add icons

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
@@ -8,13 +9,19 @@ import javafx.fxml.FXMLLoader;
 
 public class GUI extends Application implements Runnable {
     private static final String stageTitle = "SCP Controller v1.0";
+    private static final String cssRes = "flat_dark_theme.css";
+    private static final String iconRes = "/res/images/icon3.png";
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("GUIView.fxml"));
         Scene scene = new Scene(root);
         // scene.getStylesheets().clear();
-        scene.getStylesheets().add("flat_dark_theme.css");
+        scene.getStylesheets().add(cssRes);
+        stage.getIcons().add(
+                new Image(
+                        getClass().getResourceAsStream(iconRes),
+                        100, 100, true, true)); // add icon
         stage.setTitle(stageTitle);
         stage.setScene(scene);
         stage.setMaximized(true);
