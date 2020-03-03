87fcb164f66e0fc330396f7ab82c47f553c57d76
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-03-03 13:20:59 -0800

    Add fx:ids to PID text boxes

--- src/PIDMenuController.java
+++ src/PIDMenuController.java
@@ -1,13 +1,29 @@
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PIDMenuController implements Initializable {
-    private GUIModel model;
+    GUIModel model;
 
+    public TextField scp1_p_field;
+    public TextField scp1_i_field;
+    public TextField scp1_d_field;
+
+    public TextField scp2_p_field;
+    public TextField scp2_i_field;
+    public TextField scp2_d_field;
+
+    public TextField scp3_p_field;
+    public TextField scp3_i_field;
+    public TextField scp3_d_field;
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         model = GUIModel.getInstance();
+
+        // TODO: getPIDCoefficientsFromSTM32();
+        
     }
 }
